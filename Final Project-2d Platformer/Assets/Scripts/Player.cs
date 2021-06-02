fc9ae33ba2e64efb7e8e3523bd3feb1d155b49f8
SamP <sam@easkag.net>
2021-06-01 19:54:39 -0700

    added stuff

--- Final Project-2d Platformer/Assets/Scripts/Player.cs
+++ Final Project-2d Platformer/Assets/Scripts/Player.cs
@@ -5,14 +5,24 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    //config
     [SerializeField] float runSpeed = 5;
 
-    Rigidbody2D myRididBody;
+    //State
+    bool isAlive = true;
+
+
+    //Caches Ccomponent refrences
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    //message then methods
 
     // Start is called before the first frame update
     void Start()
     {
-        myRididBody = GetComponent<Rigidbody2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,16 +35,18 @@ public class Player : MonoBehaviour
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRididBody.velocity.y);
-        myRididBody.velocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        
     }
 
     private void FlipSprite()
     {
-        bool PlayerHasHorizontalSpeed = Mathf.Abs(myRididBody.velocity.x) > Mathf.Epsilon;
-        if (PlayerHasHorizontalSpeed)
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(myRididBody.velocity.x), 1f);
+            transform.localScale = new Vector2(4*Mathf.Sign(myRigidBody.velocity.x), 4f);
         }
     }
 }
