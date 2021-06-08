4d31c07766ea58fe3746ae4d614f2f7804fba7da
Samuel Payne <21.samuel.payne@ksd.org>
2021-06-08 10:21:25 -0700

    added some stuff

--- Final Project-2d Platformer/Assets/Sprites/Scripts/Menu.cs
+++ Final Project-2d Platformer/Assets/Sprites/Scripts/Menu.cs
@@ -15,6 +15,46 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    
+    public void LoadSecondLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadThirdLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadFourthLevel()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void LoadFithLevel()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void LoadSixthLevel()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void LoadSeventhLevel()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void LoadEighthLevel()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void LoadNinthLevel()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+
+
+
+
+
+
+
 
 }
